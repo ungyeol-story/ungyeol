81c436a570cb3b99b06cc7e04967bb144bf7939a
ungyeol-story <ungyeol.story@gmail.com>
2026-07-08 00:00:54 +0900

    운결 홈페이지 V2 적용

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach((link)=>{link.addEventListener("click",(event)=>{const target=document.querySelector(link.getAttribute("href"));if(!target)return;event.preventDefault();target.scrollIntoView({behavior:"smooth",block:"start"});});});
